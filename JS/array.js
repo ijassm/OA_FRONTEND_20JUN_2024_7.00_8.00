230ce604a8b588d7764f07e91eb49db7d878672f
“ijassm” <ijass.ocean@gmail.com>
2024-10-10 20:30:17 +0530

    array completed

---
+++ JS/array.js
@@ -0,0 +1,28 @@
+let names = ["merwin", "aravindh", "sadhish", "yuvasri", "devesh"];
+let capsNames = [];
+
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// console.log(names[4]);
+// console.log(names[-1]);
+// console.log(names[-2]);
+// console.log(names[-3]);
+// console.log(names.at(0));
+
+// Array.prototype.merwin = "He is good boy";
+
+// console.log(names.merwin);
+
+// ❌ Not recommended
+// for (let i in names) console.log(i);
+
+// ✔ recommended
+// for (let i of names) console.log(i);
+// for (let i = 0; i < names.length; i++) {
+//   capsNames.push(names[i].toUpperCase());
+// }
+
+// console.log(names);
+// console.log(capsNames);
